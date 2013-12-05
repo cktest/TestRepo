51ea65a334d0e90ced4cdeff3da8930d53e9017c
cktest <yzhang@crkdinc.com>
2013-12-04 17:04:03 -0800

    first check in

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, world");
+            Console.WriteLine("it is done.");
+        }
+    }
+}
